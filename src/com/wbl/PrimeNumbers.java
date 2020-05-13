fc2d9e2a10747615782017d10ddeda91fc93437f
sonia-pink <mywork.github@gmail.com>
2020-05-13 16:40:00 -0700

    First Assignment

--- src/com/wbl/PrimeNumbers.java
+++ src/com/wbl/PrimeNumbers.java
@@ -1,4 +1,27 @@
 package com.wbl;
 
 public class PrimeNumbers {
+    public static void main(String[] args) {
+
+        String primeNumbers = "";
+        System.out.println("Prime Numbers between 1 to 100 ");
+        for (int num=1; num<=100; num++)
+        {
+
+            int counter = 0;
+            for(int i=num; i>=1; i--)
+            {
+               if (num % i == 0)
+               {
+                   counter = counter + 1;
+               }
+            }
+            if(counter == 2)
+            {
+                primeNumbers = primeNumbers + num + " ";
+            }
+        }
+        System.out.println( "Prime Numbers ");
+        System.out.println(primeNumbers );
+    }
 }
